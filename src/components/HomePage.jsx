80ccb70ea1bb858dc1e388ebbff3378f0dce7287
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 06:54:03 +0100

    push

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,8 +11,10 @@ const HomePage = () => {
             <h1>The all in one content creation platform!</h1>
             <p>Create your content seamlessly as a poet, blogger and author on PenPages.</p>
           </div>
+          <Link to="/" className="home-page-btn">
+            Get Started
+          </Link>
           <img src={addContentSVG} />
-          <Link to="/">Home</Link>
         </div>
       </div>
     </div>
