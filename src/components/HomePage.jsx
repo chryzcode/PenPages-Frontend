e2dadbc5af622cfe8d999c1a440b8202f07a9129
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 16:58:00 +0100

    push

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,24 +17,34 @@ const HomePage = () => {
           <Link className="text-xl bg-customPurple py-3 px-6 text-white rounded-full" to="/">
             Get Started
           </Link>
-          <img className="w-1.2 mx-auto my-10" src={addContentSVG} />
+          <img className=" mx-auto my-10" src={addContentSVG} />
         </div>
       </div>
 
-      <div className="text-center my-32">
-        <p className="text-4xl my-10 font-semibold max-w-xl  mx-auto">
+      <div className="text-center my-28">
+        <p className="text-4xl  font-semibold max-w-xl  mx-auto">
           Publish your poem, article and e-book <span className="text-customPurple">at a go</span>.
         </p>
 
-        <div className="flex flex-nowrap space-x-56 my-20">
-          <div className="w-1.2">
-            <p className="text-3xl my-10 font-bold">
+        <div className="flex-wrap-container py-20">
+          <div className="text-center mx-auto">
+            <p className="text-3xl font-bold max-w-xl mx-auto">
               <span className="text-customPurple">No</span> content, niche and audience
               <span className="text-customPurple"> limitation</span>!
             </p>
+            <div>
+              <div className="text-left text-xl list-disc custom-list py-10 pl-28">
+                <li className="py-2">Share you knowledge in whatever field.</li>
+                <li className="py-2">No usage cost(free).</li>
+                <li className="py-2">No genre limitation.</li>
+                <li className="py-2">More content reach and audience.</li>
+              </div>
+            </div>
           </div>
 
-          <img className="w-1.2  " src={publishArticleSVG} />
+          <div className="mx-auto w">
+            <img className="w-96 mx-auto" src={publishArticleSVG} />
+          </div>
         </div>
       </div>
     </div>
