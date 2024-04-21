e2302d24ce22d151f87cd82b0e3e20e08b1be576
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 14:05:55 +0100

    push

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,18 +4,26 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <div className="block-container-center">
-        <div className="home-page-content">
-          <div>
-            <h1>The all in one content creation platform!</h1>
-            <p>Create your content seamlessly as a poet, blogger and author on PenPages.</p>
-          </div>
-          <Link to="/" className="home-page-btn">
+    <div className="mx-10">
+      <div className="text-center max-w-3xl mx-auto">
+        <div>
+          <p className="text-5xl my-10 font-semibold">
+            The <span className="text-customPurple">all in one</span> content creation platform!
+          </p>
+          <p className="text-2xl my-10 max-w-xl mx-auto">
+            Create your content seamlessly as a poet, blogger and author on PenPages.
+          </p>
+          <Link className="text-xl bg-customPurple py-3 px-6 text-white rounded-full" to="/">
             Get Started
           </Link>
-          <img src={addContentSVG} />
+          <img className="w-1.2 mx-auto my-10" src={addContentSVG} />
         </div>
+      </div>
+
+      <div className="text-center max-w-3xl mx-auto my-20">
+        <p className="text-4xl my-10 font-semibold">
+          Publish your poem, article and e-book <span className="text-customPurple">at a go</span>.
+        </p>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="main-nav">
-      <div className="flex-container">
-        <div>PenPages</div>
+      <div>PenPages</div>
 
-        <span className="nav-links">
-          <span>login</span>
-          <span>sign up</span>
-        </span>
-      </div>
+      <span className="nav-links">
+        <span>login</span>
+        <span>sign up</span>
+      </span>
     </nav>
   );
 };
