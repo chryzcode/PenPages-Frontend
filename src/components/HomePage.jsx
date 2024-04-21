8c906753771d68c358205dadc4e2a095d9b1296b
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 19:45:36 +0100

    push

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import addContentSVG from "../assets/images/add-content.svg";
 import publishArticleSVG from "../assets/images/publish-article.svg";
+import contentStructureSVG from "../assets/images/content-structure.svg";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -9,7 +10,8 @@ const HomePage = () => {
       <div className="text-center max-w-3xl mx-auto">
         <div>
           <p className="text-5xl my-10 font-semibold">
-            The <span className="text-customPurple">all in one</span> content creation platform!
+            The <span className="text-customPurple">three in one</span> content creation
+            <span className="text-customPurple"> platform</span>!
           </p>
           <p className="text-2xl my-14 max-w-xl mx-auto">
             Create your content seamlessly as a poet, blogger and author on PenPages.
@@ -17,13 +19,14 @@ const HomePage = () => {
           <Link className="text-xl bg-customPurple py-3 px-6 text-white rounded-full" to="/">
             Get Started
           </Link>
-          <img className=" mx-auto my-10" src={addContentSVG} />
+          <img className=" mx-auto my-10 home-page-image-size" src={addContentSVG} />
         </div>
       </div>
 
-      <div className="text-center my-28">
+      <div className="text-center mt-20">
         <p className="text-4xl  font-semibold max-w-xl  mx-auto">
-          Publish your poem, article and e-book <span className="text-customPurple">at a go</span>.
+          <span className="text-customPurple">Publish</span> your poem, article and e-book{" "}
+          <span className="text-customPurple">at a go</span>.
         </p>
 
         <div className="flex-wrap-container py-20">
@@ -33,7 +36,7 @@ const HomePage = () => {
               <span className="text-customPurple"> limitation</span>!
             </p>
             <div>
-              <div className="text-left text-xl list-disc custom-list py-10 pl-28">
+              <div className="text-left text-xl list-disc custom-list py-10 mx-auto w-4/5 pl-10">
                 <li className="py-2">Share you knowledge in whatever field.</li>
                 <li className="py-2">No usage cost(free).</li>
                 <li className="py-2">No genre limitation.</li>
@@ -42,8 +45,32 @@ const HomePage = () => {
             </div>
           </div>
 
-          <div className="mx-auto w">
-            <img className="w-96 mx-auto" src={publishArticleSVG} />
+          <div className="mx-auto">
+            <img className="home-page-image-size mx-auto" src={publishArticleSVG} />
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <p className="text-4xl  font-semibold mx-auto">
+          The <span className="text-customPurple">TRIO</span> content platform.
+        </p>
+
+        <div className="flex-wrap-container py-20">
+          <div className="mx-auto w-9/12">
+            <img className=" w-8/12 mx-auto" src={contentStructureSVG} />
+          </div>
+
+          <div className="text-center mx-auto">
+            <p className="text-3xl font-bold max-w-xl mx-auto">
+              <span className="text-customPurple">Seamless</span> user experience
+            </p>
+            <div className="text-left text-xl list-disc custom-list py-10 mx-auto w-4/5 pl-10">
+              <li className="py-2">Sign up with just a few clicks.</li>
+              <li className="py-2">Same text editor for different content.</li>
+              <li className="py-2">Interactive comment/ reply system.</li>
+              <li className="py-2">High content search engine optimization (SEO).</li>
+            </div>
           </div>
         </div>
       </div>
