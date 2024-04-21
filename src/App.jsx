835f21df4b21437428df74c6668fac435179fe3d
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 06:42:24 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Navbar from "./components/Navbar";
+import MainLayout from "./layout/MainLayout";
 import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <HomePage />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import addContentSVG from "../assets/images/add-content.svg";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-      <div>
+    <div>
+      <div className="block-container-center">
+        <div className="home-page-content">
           <div>
-              <img src="" alt="" />
-              <span>
-                  The all in one content creation platform
-              </span>
+            <h1>The all in one content creation platform!</h1>
+            <p>Create your content seamlessly as a poet, blogger and author on PenPages.</p>
           </div>
+          <img src={addContentSVG} />
+          <Link to="/">Home</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
