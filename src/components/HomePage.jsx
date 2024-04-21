1a09f5e74acbfced204d789d08cd8ef378ca649a
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-21 05:21:35 +0100

    push

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+      <div>
+          <div>
+              <img src="" alt="" />
+              <span>
+                  The all in one content creation platform
+              </span>
+          </div>
+    </div>
+  )
+}
+
+export default HomePage
