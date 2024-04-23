b0ce2a6c24000ed230bd5c562ffcde5b97fdb51b
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-23 06:54:32 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import "./index.css";
 import MainLayout from "./layout/MainLayout";
-import HomePage from "./components/HomePage";
+import HomePage from "../public/pages/HomePage";
+import SignUpPage from "../public/pages/SignUpPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Route>
     )
   );
