68fe5147d69c7aabaad820e77c5378a661bf8ee9
chryzcode <alabaolanrewaju13@gmail.com>
2024-04-26 12:12:48 +0100

    push

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import ClipLoader from "react-spinners/ClipLoader";
 const Spinner = ({ size, color, display }) => {
   const override = {
     zIndex: "9999",
-    display: display === "block" ? "block" : "relative",
+    display: display === "block" ? "block" : "inherit",
     margin: display === "block" ? "10% auto" : "0 auto",
   };
   return <ClipLoader color={color ? color : "white"} cssOverride={override} size={size} />;
