a17b93f2c07dbecedb072b7323b9728a2491f953
chryzcode <alabaolanrewaju13@gmail.com>
2024-07-22 09:39:34 +0100

    push

--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -8,7 +8,8 @@ const ForgotPasswordPage = () => {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  const forgotPassword = async email => {
+
+  const forgotPassword = async userEmail => {
     setIsLoading(true);
     try {
       const res = await fetch(`${API_BASE_URL}user/send-forgot-password-link`, {
@@ -16,10 +17,11 @@ const ForgotPasswordPage = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(email),
+        body: JSON.stringify(userEmail),
       });
       const data = await res.json();
-      if (res.ok) {
+
+      if (data.success) {
         toast.success("Check your mail");
         navigate("/");
       } else {
@@ -35,43 +37,35 @@ const ForgotPasswordPage = () => {
 
   const submitForm = e => {
     e.preventDefault();
-
-    const userEmail = {
-      email,
-    };
-
-    forgotPassword(email);
+    const userEmail = { email };
+    forgotPassword(userEmail);
   };
 
   return (
     <div className="mx-10">
-      <p className="text-4xl text-customPurple  font-semibold mx-auto text-center py-7">Forgot Password?</p>
-
+      <p className="text-4xl text-customPurple font-semibold mx-auto text-center py-7">Forgot Password?</p>
       <div className="flex-wrap-container py-5 align-middle px-10">
         <div>
-          <form onSubmit="">
+          <form onSubmit={submitForm}>
             <div className="my-3">
               <label htmlFor="email" className="block mb-2">
                 Email
               </label>
               <input
-                type="text"
+                type="email"
                 id="email"
                 name="email"
                 value={email}
-                onChange={e => {
-                  setEmail(e.target.value);
-                }}
+                onChange={e => setEmail(e.target.value)}
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="JDoe or johndoe@gmail.com"
                 required
               />
             </div>
-
             <div className="mx-auto w-32 my-8 text-center">
               <button
                 disabled={isLoading}
-                className="bg-customPurple hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-auto "
+                className="bg-customPurple hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-auto"
                 type="submit">
                 Submit {isLoading && <Spinner size={10} />}
               </button>
